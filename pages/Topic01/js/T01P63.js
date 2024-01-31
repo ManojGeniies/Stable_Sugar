7e6a8bb880ae605c46f5e5c6f3e2de736915ede0
Sivakumar K <105903968+sivakumar7459@users.noreply.github.com>
2024-01-31 17:32:46 +0530

    wednesday EOD

--- pages/Topic01/js/T01P63.js
+++ pages/Topic01/js/T01P63.js
@@ -73,23 +73,23 @@ var updateContentArray = [
 
 ];
 
-var updateContentArray0 = [{ time: "0.0", carry: "", event: "", show: "", hide: "", transcript: "That is correct. Click Next to continue." }
+var updateContentArray0 = [{ time: "0.0", carry: "", event: "", show: "", hide: "", transcript: "That is correct." }
 
 ];
 
-var updateContentArray1 = [{ time: "0.0", carry: "", event: "", show: "", hide: "", transcript: "Here is some feedback on your response. See the options highlighted in yellow that are appropriate actions to be taken. Click Next to continue." }
+var updateContentArray1 = [{ time: "0.0", carry: "", event: "", show: "", hide: "", transcript: "Here is some feedback on your response. See the options highlighted in yellow that are appropriate actions to be taken." }
 
 ];
 
-var updateContentArray2 = [{ time: "0.0", carry: "", event: "", show: "", hide: "", transcript: "Here is some feedback on your response. See the options highlighted in yellow that are appropriate actions to be taken. Click Next to continue." }
+var updateContentArray2 = [{ time: "0.0", carry: "", event: "", show: "", hide: "", transcript: "Here is some feedback on your response. See the options highlighted in yellow that are appropriate actions to be taken." }
 
 ];
 
-var updateContentArray3 = [{ time: "0.0", carry: "", event: "", show: "", hide: "", transcript: "Here is some feedback on your response. See the options highlighted in yellow that are appropriate actions to be taken. Click Next to continue." }
+var updateContentArray3 = [{ time: "0.0", carry: "", event: "", show: "", hide: "", transcript: "Here is some feedback on your response. See the options highlighted in yellow that are appropriate actions to be taken." }
 
 ];
 
-var updateContentArray4 = [{ time: "0.0", carry: "", event: "", show: "", hide: "", transcript: "Here is some feedback on your response. See the options highlighted in yellow that are appropriate actions to be taken. Click Next to continue." }
+var updateContentArray4 = [{ time: "0.0", carry: "", event: "", show: "", hide: "", transcript: "Here is some feedback on your response. See the options highlighted in yellow that are appropriate actions to be taken." }
 
 ];
 
@@ -105,7 +105,7 @@ var __dataXml;
 
 var audplayer = document.getElementById('audioplayer');
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     disableNextBtn();
     clickViewMoreInfo();
@@ -248,7 +248,7 @@ function bindClickableHotspot() {
 
     $('.option').addClass('aactive'); //.css({ 'cursor': 'pointer' });
 
-    $('.option').off().on('click', function(e) {
+    $('.option').off().on('click', function (e) {
         e.preventDefault();
 
         $(this).toggleClass('clicked');
@@ -257,7 +257,7 @@ function bindClickableHotspot() {
 
             $('.cSubmitBtn').addClass('active');
 
-            $('.cSubmitBtn.active').off().on('click', function() {
+            $('.cSubmitBtn.active').off().on('click', function () {
                 submitClick($(this));
             });
         } else {
@@ -303,7 +303,7 @@ function areDifferentByIds(clickVal, correctVal) {
 
 function highlightBorderPushVal() {
     __clickedOpt = [];
-    $(".option.clicked").each(function() {
+    $(".option.clicked").each(function () {
         console.log("highlight " + $(this).attr("curid"));
         __clickedOpt.push($(this).attr("curid"));
     });
@@ -325,7 +325,7 @@ function submitClick(aCurrent) {
         var incorrectCount = 0,
             correctCount = 0;
 
-        $('.option').each(function() {
+        $('.option').each(function () {
             console.log($(this).index());
 
 
@@ -354,7 +354,7 @@ function submitClick(aCurrent) {
 
 
 
-        $(".feedback_icon").off("click").on("click", function() {
+        $(".feedback_icon").off("click").on("click", function () {
             fnPauseAudio();
             var _getDesc = '';
             var _getHeading = '';
@@ -378,7 +378,7 @@ function submitClick(aCurrent) {
         });
         $(".feedback_icon").show();
 
-        $(correctOptions).each(function(i, val) {
+        $(correctOptions).each(function (i, val) {
             $('.option').eq(val).removeClass("wrong").addClass('correct');
         });
 
@@ -440,7 +440,7 @@ function submitClick(aCurrent) {
 
 
 
-$(".small_expand").on("click", function() {
+$(".small_expand").on("click", function () {
 
     pauseAudio();
 
@@ -477,7 +477,7 @@ $(".small_expand").on("click", function() {
 
 });
 
-$(".closepopup").on("click", function() {
+$(".closepopup").on("click", function () {
 
     playAudio();
 
